2a19f8a0a4af41de5d8f889fa1eea2651f964569
DostynL <dostynlpz@gmail.com>
2026-05-29 02:50:04 -0600

    Configs Neo4j

---
+++ config.py
@@ -0,0 +1,8 @@
+# =============================================================
+#  config.py — Configuración de conexión a Neo4j
+#  Sistema de Recomendaciones de Videos
+# =============================================================
+
+NEO4J_URI      = "neo4j://127.0.0.1:7687"
+NEO4J_USER     = "neo4j"
+NEO4J_PASSWORD = "1234hola"   # Cambia esto si usas otra contraseña
